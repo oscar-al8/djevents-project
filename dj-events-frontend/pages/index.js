ee6056498d319329b0f45911ad9ef09361d4a249
oscar_al8 <oscarmehrer@hotmail.com>
2021-09-13 14:22:58 -0400

    Link changed to link

--- dj-events-frontend/pages/index.js
+++ dj-events-frontend/pages/index.js
@@ -1,4 +1,4 @@
-import Link from 'next/Link';
+import Link from 'next/link';
 import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
 import { API_URL }  from '@/config/index';
